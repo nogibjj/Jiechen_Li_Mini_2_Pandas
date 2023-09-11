33de0aeea4a85f7464cb2363b7934173340c69f3
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-11 02:11:39 +0000

    new requirement first commit

---
+++ test_main.py
@@ -0,0 +1,14 @@
+"""
+Test goes here
+
+"""
+
+from main import csvfile
+
+def test_csvfile():
+    main_data = "titanic_main_data.csv"
+    result = csvfile(main_data)
+
+if __name__ == "__main__":
+    test_csvfile()
+
