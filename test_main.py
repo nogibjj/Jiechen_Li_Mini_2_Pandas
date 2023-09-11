9508e751c2b66876aeacb31c4e73500d8bfd8791
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-11 02:41:58 +0000

    final commit

--- test_main.py
+++ test_main.py
@@ -5,10 +5,11 @@ Test goes here
 
 from main import csvfile
 
+
 def test_csvfile():
     main_data = "titanic_main_data.csv"
     result = csvfile(main_data)
 
+
 if __name__ == "__main__":
     test_csvfile()
-
