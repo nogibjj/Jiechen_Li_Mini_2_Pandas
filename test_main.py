2269b2da13cbbf2f50d530cf90f0ee2f29d34f21
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-13 15:53:35 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,14 +10,14 @@ def test_csvfile():
     main_data = "titanic_main_data.csv"
     result = csvfile(main_data)
     
-    # mean value of the 1st column resting blood pressure
+    # mean value of the 1st column
     mean_1_column = test_csvfile.iloc[:, 1]["mean"]
-    # print(mean_4_column)
+    # print(mean_1_column)
     assert mean_1_column == 446
-    # standard deviation value of fourth column resting blood pressure
-    std_4_column = summary_result.iloc[:, 1]["min"]
-    # print(std_4_column)
-    assert std_1_column == 1
+    # min value of 1st column 
+    min_4_column = summary_result.iloc[:, 1]["min"]
+    # print(min_1_column)
+    assert min_1_column == 1
 
 
 if __name__ == "__main__":
