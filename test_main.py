a2699373f53b209343646cb3cc83464be56fd08a
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-13 16:15:42 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,13 +11,13 @@ def test_csvfile():
     result = csvfile(main_data)
     
     # mean value of the 1st column
-    mean_1_column = result.iloc[:, 1].mean()
+    mean_1_column = result.iloc[:, 0].mean()
     # print(mean_1_column)
     assert mean_1_column == 446
     # min value of 1st column 
-    # min_4_column = result.iloc[:, 1]["min"]
-    # # print(min_1_column)
-    # assert min_1_column == 1
+    min_4_column = result.iloc[:, 0].min()
+    # print(min_1_column)
+    assert min_1_column == 1
 
 
 if __name__ == "__main__":
