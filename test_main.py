8ea70c1ea4c23563c2aeb924aba3c5da1ab2e18f
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-13 15:49:39 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -11,13 +11,13 @@ def test_csvfile():
     result = csvfile(main_data)
     
     # mean value of the 1st column resting blood pressure
-    mean_1_column = test_csvfile.iloc[:, 3]["mean"]
+    mean_1_column = test_csvfile.iloc[:, 1]["mean"]
     # print(mean_4_column)
-    assert mean_1_column == 
+    assert mean_1_column == 446
     # standard deviation value of fourth column resting blood pressure
-    std_4_column = summary_result.iloc[:, 3]["std"]
+    std_4_column = summary_result.iloc[:, 1]["min"]
     # print(std_4_column)
-    assert std_4_column == 17.5381428135171
+    assert std_1_column == 1
 
 
 if __name__ == "__main__":
